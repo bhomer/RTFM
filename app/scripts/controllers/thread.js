3790787f74ea7d9eefb268f85bdb8086737d5ba3
Benjamin <benjaminhomer@gmail.com>
2014-03-01 12:51:50 -0700

    set up firebase and threads

--- app/scripts/controllers/thread.js
+++ app/scripts/controllers/thread.js
@@ -4,9 +4,15 @@ angular.module('rtfmApp', ['firebase'])
   .controller('ThreadCtrl', function ($scope, $firebase) {
       
       var threadsRef = new Firebase('http://devmtn-ftfm.firebaseIO.com/threads');
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+      
+      $scope.threads = $firebase(threadsRef);
+      $scope.currentThread;
+
+      $scope.addThread = function(e) {
+        if (e.keyCode !== 13) {
+          return;
+      }
+      
+      $scope.threads.$add({name: $scope.newThreadTitle});
+      $scope.newThreadTitle = '';
+  };
